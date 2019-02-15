76e400b91c55a3bf4407452cec96c6d7b509ca83
Pan Hezeng <panhezeng@gmail.com>
2019-02-15 15:01:19 +0800

    优化依赖模块版本

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,79 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
   output: {
-    filename: 'el-multiple-upload.min.js',
-    libraryTarget: 'umd',
+    filename: "el-multiple-upload.min.js",
+    libraryTarget: "umd",
     umdNamedDefine: true,
-    library: 'ElMultipleUpload',
-    libraryExport: 'ElMultipleUpload'
+    library: "ElMultipleUpload",
+    libraryExport: "ElMultipleUpload"
   },
   externals: {
-    '@panhezeng/utils/dist/check-upload.js': '@panhezeng/utils/dist/check-upload.js',
-    '@panhezeng/utils/dist/get-object-item-by-path.js': '@panhezeng/utils/dist/get-object-item-by-path.js',
-    'vue': 'Vue',
-    'element-ui': {
-      commonjs: 'element-ui',
-      commonjs2: 'element-ui',
-      amd: 'element-ui',
-      root: 'ELEMENT'
+    "@panhezeng/utils/dist/check-upload.js": {
+      commonjs: "@panhezeng/utils/dist/check-upload.js",
+      commonjs2: "@panhezeng/utils/dist/check-upload.js",
+      amd: "@panhezeng/utils/dist/check-upload.js",
+      root: "checkUpload"
+    },
+    "@panhezeng/utils/dist/get-object-item-by-path.js": {
+      commonjs: "@panhezeng/utils/dist/get-object-item-by-path.js",
+      commonjs2: "@panhezeng/utils/dist/get-object-item-by-path.js",
+      amd: "@panhezeng/utils/dist/get-object-item-by-path.js",
+      root: "getObjectItemByPath"
+    },
+    vue: {
+      commonjs: "vue",
+      commonjs2: "vue",
+      amd: "vue",
+      root: "Vue"
+    },
+    "element-ui": {
+      commonjs: "element-ui",
+      commonjs2: "element-ui",
+      amd: "element-ui",
+      root: "ELEMENT"
     }
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        use: 'vue-loader'
+        use: "vue-loader"
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: "babel-loader"
       },
       {
         test: /\.(css|less)$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'less-loader'
-        ]
+        use: ["vue-style-loader", "css-loader", "less-loader"]
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: 'img/[name].[hash:7].[ext]'
+          name: "img/[name].[hash:7].[ext]"
         }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: 'media/[name].[hash:7].[ext]'
+          name: "media/[name].[hash:7].[ext]"
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: 'fonts/[name].[hash:7].[ext]'
+          name: "fonts/[name].[hash:7].[ext]"
         }
       }
     ]
   },
-  plugins: [
-    new VueLoaderPlugin()
-  ]
-}
+  plugins: [new VueLoaderPlugin()]
+};
