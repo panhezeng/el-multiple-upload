dbff8c42176c7afc6863d1cf5a0156a64e273184
Pan Hezeng <panhezeng@gmail.com>
2019-07-25 16:03:14 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-export const ElMultipleUpload = require('./ElMultipleUpload.vue').default
+export const ElMultipleUpload = require("./ElMultipleUpload.vue").default;
 
-let Vue
+let Vue;
 
-function install (_Vue, options = (typeof window !== 'undefined' && window.ElMultipleUploadOptions)) {
-
+function install(
+  _Vue,
+  options = typeof window !== "undefined" && window.ElMultipleUploadOptions
+) {
   if (Vue) {
-    console.warn('[ElMultipleUpload] already installed. Vue.use(ElMultipleUpload) should be called only once.')
-    return
+    console.warn(
+      "[ElMultipleUpload] already installed. Vue.use(ElMultipleUpload) should be called only once."
+    );
+    return;
   }
 
-  Vue = _Vue
+  Vue = _Vue;
 
-  if (Object.prototype.toString.call(options) === '[object Object]') {
-    if (Object.prototype.toString.call(options.upload) === '[object Function]') {
+  if (Object.prototype.toString.call(options) === "[object Object]") {
+    if (
+      /^\[object [^F]*Function\]$/.test(
+        Object.prototype.toString.call(options.upload)
+      )
+    ) {
       Object.assign(ElMultipleUpload.props.upload, {
         required: false,
         default: options.upload
-      })
+      });
     }
   }
 
-  Vue.component(ElMultipleUpload.name, ElMultipleUpload)
+  Vue.component(ElMultipleUpload.name, ElMultipleUpload);
 }
 
 /* istanbul ignore next */
-ElMultipleUpload.install = install
+ElMultipleUpload.install = install;
 
 // auto install in dist mode
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
 }
-
-
